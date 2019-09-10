ca3b71ce7ce798fb32121e5006be7c0cb9a7f01c
Leboeuf Fabien <F.Leboeuf@salford.ac.uk>
2019-09-10 15:52:03 +0200

    setup.py created

---
+++ setup.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup,find_packages
+
+#------------------------- INSTALL--------------------------------------------
+setup(name = 'qtmWebGaitReport',
+    version = "0.0.1",
+    author = 'Fabien Leboeuf',
+    author_email = 'fabien.leboeuf@gmail.com',
+    description = "fork of the qtm web report uploader ",
+    keywords = 'gait, mocap',
+    packages=find_packages(),
+	include_package_data=True,
+    license='MIT License',
+    classifiers=['Development Status :: 4 - Beta',
+                 'Programming Language :: Python',
+                 'Programming Language :: Python :: 2.7',
+                 'Operating System :: Microsoft :: Windows',
+                 'Natural Language :: English'],
+    )
