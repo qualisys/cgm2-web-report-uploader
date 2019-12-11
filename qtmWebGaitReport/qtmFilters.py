04ce2103bc4b0584abeb449ab1d55526ae7ac42d
Sten Remmelg <sten.remmelg@gmail.com>
2019-12-11 15:48:23 +0100

    raise exception when config.json file is not found

--- qtmWebGaitReport/qtmFilters.py
+++ qtmWebGaitReport/qtmFilters.py
@@ -6,27 +6,26 @@ from qtmWebGaitReport import parserUploader
 
 
 class WebReportFilter(object):
-    def __init__(self,workingDirectory,modelledC3dfilenames,subjectInfo,sessionDate):
-
-        if os.path.isfile(qtmWebGaitReport.GAIT_WEB_REPORT_PATH + 'config.json'):
-            with open(qtmWebGaitReport.GAIT_WEB_REPORT_PATH + 'config.json') as jsonDataFile:
+    def __init__(self, workingDirectory, modelledC3dfilenames, subjectInfo, sessionDate):
+        configPath = os.path.join(
+            qtmWebGaitReport.GAIT_WEB_REPORT_PATH, 'config.json')
+        if os.path.isfile(configPath):
+            with open(configPath) as jsonDataFile:
                 configData = json.load(jsonDataFile)
         else:
-            print "Config.json not found at " + os.getcwd()
-
+            raise Exception("Config.json not found at " +
+                            qtmWebGaitReport.GAIT_WEB_REPORT_PATH)
 
         self.processing = parserUploader.ParserUploader(workingDirectory,
-                    configData,modelledC3dfilenames,
-                    subjectInfo,sessionDate)
-
+                                                        configData, modelledC3dfilenames,
+                                                        subjectInfo, sessionDate)
 
     def exportJson(self):
 
         jsonData = self.processing.createReportJson()
 
         with open(str(""+"jsonData.json"), 'w') as outfile:
-            json.dump(jsonData, outfile,indent=4)
-
+            json.dump(jsonData, outfile, indent=4)
 
     def upload(self):
         self.processing.Upload()
