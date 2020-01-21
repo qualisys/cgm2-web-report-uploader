575b5608dc58d624c05d1f98d67ee6f9bc9cba00
Sten Remmelg <sten.remmelg@gmail.com>
2020-01-21 11:47:37 +0100

    change setup.py to include contact info for qualisys

--- setup.py
+++ setup.py
@@ -1,21 +1,21 @@
 # -*- coding: utf-8 -*-
-from setuptools import setup,find_packages
+from setuptools import setup, find_packages
 
-#------------------------- INSTALL--------------------------------------------
-setup(name = 'qtmWebGaitReport',
-    version = "0.0.1",
-    author = 'Fabien Leboeuf',
-    author_email = 'fabien.leboeuf@gmail.com',
-    description = "fork of the qtm web report uploader ",
-    keywords = 'gait, mocap',
-    packages=find_packages(),
-	include_package_data=True,
-    license='MIT License',
-    install_requires = ['ffmpy>=0.2.2',
+# ------------------------- INSTALL--------------------------------------------
+setup(name='qtmWebGaitReport',
+      version="0.0.1",
+      author='Qualisys',
+      author_email='support@qualisys.com',
+      description="qtm web report uploader ",
+      keywords='gait, mocap',
+      packages=find_packages(),
+      include_package_data=True,
+      license='MIT License',
+      install_requires=['ffmpy>=0.2.2',
                         'requests>=2.7.0'],
-    classifiers=['Development Status :: 4 - Beta',
-                 'Programming Language :: Python',
-                 'Programming Language :: Python :: 2.7',
-                 'Operating System :: Microsoft :: Windows',
-                 'Natural Language :: English'],
-    )
+      classifiers=['Development Status :: 4 - Beta',
+                   'Programming Language :: Python',
+                   'Programming Language :: Python :: 2.7',
+                   'Operating System :: Microsoft :: Windows',
+                   'Natural Language :: English'],
+      )
