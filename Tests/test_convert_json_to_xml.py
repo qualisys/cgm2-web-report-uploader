4ff01f31c5b6bf26e0b36ae47a15f0a481a4b4fd
Sten Remmelg <sten.remmelg@gmail.com>
2020-02-12 11:35:17 +0100

    rename test data file

--- Tests/test_convert_json_to_xml.py
+++ Tests/test_convert_json_to_xml.py
@@ -1,4 +1,4 @@
-from qtmWebGaitReport.convert_report_json_to_regression_test_xml import get_xml_string_from_report_json
+from qtmWebGaitReport.convert_report_json_to_regression_test_xml import get_xml_string_from
 from qtmWebGaitReport.convert_report_json_to_regression_test_xml import dict_to_xml
 import os
 import json
@@ -13,7 +13,7 @@ with open(report_json_path, "r") as f:
 
 def load_previous_session_data_xml():
     path_to_previous_session_data = os.path.join(
-        "TestFiles", "ClinicalGaitExample", "session_data.xml")
+        "TestFiles", "ClinicalGaitExample", "example_session_data.xml")
     with open(path_to_previous_session_data, "r") as f:
         previous_report_xml_string = f.read()
     return previous_report_xml_string
@@ -25,10 +25,5 @@ def previous_report_xml_string():
 
 
 def test_check_that_xml_string_has_not_changed(previous_report_xml_string):
-    report_xml_string = get_xml_string_from_report_json(report_json_data)
+    report_xml_string = get_xml_string_from(report_json_data)
     assert report_xml_string == previous_report_xml_string, "xml string has changed"
-
-    # # save xml to file
-    # xml_filename = "session_data.xml"
-    # with open(xml_filename, "w") as f:
-    #     f.write(report_xml_string)
