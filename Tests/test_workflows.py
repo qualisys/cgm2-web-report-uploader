7a1bebc178414742343ab42c4e6da556440f70e7
Sten Remmelg <sten.remmelg@gmail.com>
2020-02-20 15:55:14 +0100

    add test for event detector, this test opens mokka window that needs to be closed manually

--- Tests/test_workflows.py
+++ Tests/test_workflows.py
@@ -1,8 +1,11 @@
 from qtmWebGaitReport.CGM_workflow_main import CGM1_workflow
 from qtmWebGaitReport.CGM_workflow_main import CGM23_workflow
+from qtmWebGaitReport.EventDetector_Zeni import run_event_detection_and_verify_in_mokka
 from qtmWebGaitReport.utils import read_session_xml
+from qtmWebGaitReport.utils import create_directory_if_needed
 
 import os
+import glob
 import pytest
 
 session_xml_file_name = "session.xml"
@@ -21,9 +24,39 @@ def session_xml(clinical_gait_example_work_folder):
 
 class TestCGM1Workflow:
     def test_runs_without_errors(self, session_xml, clinical_gait_example_work_folder):
+        # TODO running this test updates the processed c3d files which is unwanted
         CGM1_workflow(session_xml, clinical_gait_example_work_folder)
         assert 1, "When this assertion is run all is fine and dandy"
 
 
-def test_CGM2_workflow():
-    assert 0
+class TestEventDetection:
+    def test_zeni_flow(self, clinical_gait_example_work_folder):
+        processed_folder_name = "with_events_test_generated"
+        processed_to_compare_to = "with_events"
+        # run the detection
+        run_event_detection_and_verify_in_mokka(
+            clinical_gait_example_work_folder, processed_folder_name)
+        # compare to presaved c3ds
+        presaved_events_folder = os.path.join(
+            clinical_gait_example_work_folder, processed_to_compare_to)
+        generated_events_folder = os.path.join(
+            clinical_gait_example_work_folder, processed_folder_name)
+        for c3d_path in glob.glob(os.path.join(presaved_events_folder, "*.c3d")):
+            c3d_name = os.path.split(c3d_path)[-1]
+            generated_c3d_path = os.path.join(
+                generated_events_folder, c3d_name)
+            exists_in_generated = os.path.isfile(generated_c3d_path)
+            # the file needs to exist
+            assert exists_in_generated, "Did not find %s in %s" % (
+                c3d_name, generated_events_folder)
+            # and it needs to be the same as before
+            with open(c3d_path, "rb") as presaved_c3d:
+                with open(generated_c3d_path, "rb") as generated_c3d:
+                    assert generated_c3d.read() == presaved_c3d.read(
+                    ), "generated event c3d differs from presaved one"
+
+            # cleanup generated file
+            os.remove(generated_c3d_path)
+
+# def test_CGM2_workflow():
+#     assert 0
