1a5b903deb35c070806bda31c9c30500fc7fd3f3
Sten Remmelg <sten.remmelg@gmail.com>
2020-02-20 14:31:54 +0100

    lift more funcitonality into separate funcitons

--- qtmWebGaitReport/CGM_workflow_main.py
+++ qtmWebGaitReport/CGM_workflow_main.py
@@ -107,45 +107,7 @@ def create_pdf_report(data_path, modelled_trials, title, model, point_suffix, no
     plt.show()
 
 
-def CGM1_workflow(sessionXML, work_folder):
-    sessionDate = utils.get_creation_date(sessionXML)
-    # ---------------------------------------------------------------------------
-    # management of the Processed folder
-    DATA_PATH = os.path.join(work_folder, "processed")
-    files.createDir(DATA_PATH)
-
-    staticMeasurement = qtmTools.findStatic(sessionXML)
-    filename = qtmTools.getFilename(staticMeasurement)
-    calibrated_file_path = os.path.join(DATA_PATH, filename)
-    if not os.path.isfile(calibrated_file_path):
-        shutil.copyfile(os.path.join(work_folder, filename),
-                        calibrated_file_path)
-        logging.info("qualisys exported c3d file [%s] copied to processed folder" % (
-            filename))
-
-    dynamicMeasurements = qtmTools.findDynamic(sessionXML)
-    for dynamicMeasurement in dynamicMeasurements:
-        filename = qtmTools.getFilename(dynamicMeasurement)
-        no_events_file_path = os.path.join(work_folder, filename)
-        processed_file_path = os.path.join(DATA_PATH, filename)
-        if not os.path.isfile(processed_file_path):
-            shutil.copyfile(no_events_file_path,
-                            processed_file_path)
-            logging.info("qualisys exported c3d file [%s] copied to processed folder" % (
-                filename))
-
-            acq = btkTools.smartReader(processed_file_path)
-            if "5" in btkTools.smartGetMetadata(acq, "FORCE_PLATFORM", "TYPE"):
-                forceplates.correctForcePlateType5(acq)
-            acq = eventDetector.zeni(acq)
-            btkTools.smartWriter(acq, processed_file_path)
-
-            cmd = "Mokka.exe \"%s\"" % (processed_file_path)
-            os.system(cmd)
-
-    # --------------------------GLOBAL SETTINGS ------------------------------------
-    # global setting ( in user/AppData)
-
+def fetch_translators(work_folder):
     if os.path.isfile(pyCGM2.PYCGM2_APPDATA_PATH + "CGM1-pyCGM2.settings"):
         settings = files.openFile(
             pyCGM2.PYCGM2_APPDATA_PATH, "CGM1-pyCGM2.settings")
@@ -157,20 +119,18 @@ def CGM1_workflow(sessionXML, work_folder):
     translators = files.getTranslators(work_folder+"\\", "CGM1.translators")
     if not translators:
         translators = settings["Translators"]
+    return translators
 
-    # --------------------------MP ------------------------------------
-    required_mp, optional_mp = qtmTools.SubjectMp(sessionXML)
 
-    # --------------------------MODEL CALIBRATION -----------------------
+def get_model_and_fit_to_measurements(sessionXML, data_path, translators, required_mp, optional_mp, point_suffix):
     staticMeasurement = qtmTools.findStatic(sessionXML)
     calibrateFilenameLabelled = qtmTools.getFilename(staticMeasurement)
     leftFlatFoot = toBool(staticMeasurement.Left_foot_flat)
     rightFlatFoot = toBool(staticMeasurement.Right_foot_flat)
     headFlat = toBool(staticMeasurement.Head_flat)
     markerDiameter = float(staticMeasurement.Marker_diameter.text)*1000.0
-    point_suffix = None
 
-    model, _ = cgm1.calibrate(DATA_PATH + "\\", calibrateFilenameLabelled, translators,
+    model, _ = cgm1.calibrate(data_path + "\\", calibrateFilenameLabelled, translators,
                               required_mp, optional_mp,
                               leftFlatFoot, rightFlatFoot, headFlat, markerDiameter,
                               point_suffix)
@@ -187,7 +147,7 @@ def CGM1_workflow(sessionXML, work_folder):
         momentProjection = enums.MomentProjection.Distal
 
         # filtering
-        file_path = os.path.join(DATA_PATH, filename)
+        file_path = os.path.join(data_path, filename)
         acq = btkTools.smartReader(file_path)
         if "5" in btkTools.smartGetMetadata(acq, "FORCE_PLATFORM", "TYPE"):
             forceplates.correctForcePlateType5(acq)
@@ -211,7 +171,7 @@ def CGM1_workflow(sessionXML, work_folder):
 
         btkTools.smartWriter(acq, file_path)
 
-        acqGait = cgm1.fitting(model, DATA_PATH + "\\", filename,
+        acqGait = cgm1.fitting(model, data_path + "\\", filename,
                                translators,
                                markerDiameter,
                                point_suffix,
@@ -219,44 +179,111 @@ def CGM1_workflow(sessionXML, work_folder):
 
         btkTools.smartWriter(acqGait, file_path)
         modelledC3ds.append(filename)
+    return model
 
-    # --------------------------GAIT PROCESSING -----------------------
-    webReportFlag = toBool(str(sessionXML.find("Create_WEB_report").text))
-    pdfReportFlag = toBool(str(sessionXML.find("Create_PDF_report").text))
 
-    if webReportFlag or pdfReportFlag:
-        normative_dataset = normativeDatasets.Schwartz2008("Free")
+def copy_files_to_processed_folder(sessionXML, work_folder, data_path):
+    staticMeasurement = qtmTools.findStatic(sessionXML)
+    filename = qtmTools.getFilename(staticMeasurement)
+    calibrated_file_path = os.path.join(data_path, filename)
+    if not os.path.isfile(calibrated_file_path):
+        shutil.copyfile(os.path.join(work_folder, filename),
+                        calibrated_file_path)
+        logging.info("qualisys exported c3d file [%s] copied to processed folder" % (
+            filename))
 
-        measurement_types = qtmTools.detectMeasurementType(sessionXML)
-        for measurement_type in measurement_types:
+    dynamicMeasurements = qtmTools.findDynamic(sessionXML)
+    for dynamicMeasurement in dynamicMeasurements:
+        filename = qtmTools.getFilename(dynamicMeasurement)
+        no_events_file_path = os.path.join(work_folder, filename)
+        processed_file_path = os.path.join(data_path, filename)
+        if not os.path.isfile(processed_file_path):
+            shutil.copyfile(no_events_file_path,
+                            processed_file_path)
+            logging.info("qualisys exported c3d file [%s] copied to processed folder" % (
+                filename))
 
-            modelledTrials = []
-            for dynamicMeasurement in dynamicMeasurements:
-                if qtmTools.isType(dynamicMeasurement, measurement_type):
-                    filename = qtmTools.getFilename(dynamicMeasurement)
-                    modelledTrials.append(filename)
+            acq = btkTools.smartReader(processed_file_path)
+            if "5" in btkTools.smartGetMetadata(acq, "FORCE_PLATFORM", "TYPE"):
+                forceplates.correctForcePlateType5(acq)
+            acq = eventDetector.zeni(acq)
+            btkTools.smartWriter(acq, processed_file_path)
 
-            subjectMd = {"patientName": sessionXML.find("Last_name").text + " " + sessionXML.find("First_name").text,
-                         "bodyHeight": sessionXML.find("Height").text,
-                         "bodyWeight": sessionXML.find("Weight").text,
-                         "diagnosis": sessionXML.find("Diagnosis").text,
-                         "dob": sessionXML.find("Date_of_birth").text,
-                         "sex": sessionXML.find("Sex").text,
-                         "test condition": measurement_type,
-                         "gmfcs": sessionXML.find("Gross_Motor_Function_Classification").text,
-                         "fms": sessionXML.find("Functional_Mobility_Scale").text}
+            cmd = "Mokka.exe \"%s\"" % (processed_file_path)
+            os.system(cmd)
 
-            if webReportFlag:
-                report = qtmFilters.WebReportFilter(
-                    DATA_PATH, modelledTrials, subjectMd, sessionDate)
-                # report.exportJson()
-                save_session_data_xml_from(report.getReportData())
-                report.upload()
-                logging.info("qualisys Web Report exported")
 
-            if pdfReportFlag:
-                create_pdf_report(
-                    DATA_PATH, modelledTrials, measurement_type, model, point_suffix, normative_dataset)
+def get_modelled_trials(session_xml, measurement_type):
+    modelled_trials = []
+    dynamicMeasurements = qtmTools.findDynamic(session_xml)
+    for dynamicMeasurement in dynamicMeasurements:
+        if qtmTools.isType(dynamicMeasurement, measurement_type):
+            filename = qtmTools.getFilename(dynamicMeasurement)
+            modelled_trials.append(filename)
+    return modelled_trials
+
+
+def create_web_report(session_xml, data_path):
+
+    measurement_types = qtmTools.detectMeasurementType(session_xml)
+    for measurement_type in measurement_types:
+
+        modelledTrials = get_modelled_trials(session_xml, measurement_type)
+
+        subjectMd = {"patientName": session_xml.find("Last_name").text + " " + session_xml.find("First_name").text,
+                     "bodyHeight": session_xml.find("Height").text,
+                     "bodyWeight": session_xml.find("Weight").text,
+                     "diagnosis": session_xml.find("Diagnosis").text,
+                     "dob": session_xml.find("Date_of_birth").text,
+                     "sex": session_xml.find("Sex").text,
+                     "test condition": measurement_type,
+                     "gmfcs": session_xml.find("Gross_Motor_Function_Classification").text,
+                     "fms": session_xml.find("Functional_Mobility_Scale").text}
+
+        sessionDate = utils.get_creation_date(session_xml)
+
+        report = qtmFilters.WebReportFilter(
+            data_path, modelledTrials, subjectMd, sessionDate)
+        # report.exportJson()
+        save_session_data_xml_from(report.getReportData())
+        report.upload()
+        logging.info("qualisys Web Report exported")
+
+
+def create_pdf_report(session_xml, data_path, model, point_suffix):
+    normative_dataset = normativeDatasets.Schwartz2008("Free")
+    measurement_types = qtmTools.detectMeasurementType(session_xml)
+    for measurement_type in measurement_types:
+
+        modelledTrials = get_modelled_trials(session_xml, measurement_type)
+        create_pdf_report(
+            data_path, modelledTrials, measurement_type, model, point_suffix, normative_dataset)
+
+
+def CGM1_workflow(session_xml, work_folder):
+
+    # ---------------------------------------------------------------------------
+    # management of the Processed folder
+    data_path = os.path.join(work_folder, "processed")
+    files.createDir(data_path)
+
+    copy_files_to_processed_folder(session_xml, work_folder, data_path)
+
+    translators = fetch_translators(work_folder)
+    required_mp, optional_mp = qtmTools.SubjectMp(session_xml)
+    point_suffix = None
+
+    model = get_model_and_fit_to_measurements(
+        session_xml, data_path, translators, required_mp, optional_mp, point_suffix)
+
+    # --------------------------Reporting -----------------------
+    webReportFlag = toBool(str(session_xml.find("Create_WEB_report").text))
+    pdfReportFlag = toBool(str(session_xml.find("Create_PDF_report").text))
+
+    if webReportFlag:
+        create_web_report(session_xml)
+    if pdfReportFlag:
+        create_pdf_report(session_xml, data_path, model, point_suffix)
 
 
 def CGM23_workflow(sessionXML):
