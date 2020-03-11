9b59b1d6a126f0a337df0a92f199db9b0f94a862
Sten Remmelg <sten.remmelg@gmail.com>
2020-03-11 11:56:50 +0100

    update argument name

--- main.py
+++ main.py
@@ -2,9 +2,9 @@ from qtmWebGaitReport.CGM_workflow_main import main as CGM_workflow_main
 import argparse
 
 parser = argparse.ArgumentParser()
-parser.add_argument("--settings-php-path",
+parser.add_argument("--templates-path",
                     default="",
-                    help="Give path to settings.php file to load some settings from the file")
+                    help="Give path to Templates directory")
 args = parser.parse_args()
 
 if __name__ == "__main__":
