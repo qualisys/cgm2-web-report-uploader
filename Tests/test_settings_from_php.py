d64b91c065d9ffb926d326f63c9faff915ee1192
Sten Remmelg <sten.remmelg@gmail.com>
2020-05-13 11:11:17 +0200

    update test import

--- Tests/test_settings_from_php.py
+++ Tests/test_settings_from_php.py
@@ -1,11 +1,11 @@
-from qtmWebGaitReport.pyCGM_workflows.reporting import load_settings_from_php
+from qtmWebGaitReport.CGM_workflow_main import __load_settings_from_php
 from pathlib2 import Path
 
 path_to_settings_php = Path("TestFiles") / "settings.php"
 
 
 def test_load_settings():
-    settings_dict = load_settings_from_php(str(path_to_settings_php))
+    settings_dict = __load_settings_from_php(str(path_to_settings_php))
     key_value_pairs_to_check = {"force_threshold": 10}
     for key_to_check, value_to_check in key_value_pairs_to_check.items():
         assert key_to_check in settings_dict.keys(
