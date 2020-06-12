231b885719f92ce47bf862a2b8b180aaa6e72148
Sten Remmelg <sten.remmelg@gmail.com>
2020-06-12 11:50:33 +0200

    remove obsolete test case

--- Tests/test_workflows.py
+++ Tests/test_workflows.py
@@ -4,9 +4,6 @@ import os
 from distutils.dir_util import copy_tree
 import pytest
 
-with_fixed_marker_names_folder = str(
-    Path("TestFiles", "WithFixedMarkerNames").absolute())
-
 
 @pytest.fixture(params=["CGM1.0", "CGM1.1", "CGM2.1-HJC", "CGM2.2-IK", "CGM2.3-skinClusters", "CGM2.4-ForeFoot"])
 def model_type(request):
@@ -15,15 +12,6 @@ def model_type(request):
 def copy_folder_contents(src_dir,dst_dir):
     copy_tree(src_dir,dst_dir)
 
-
-def test_runs_without_errors( model_type,tmp_path ):
-    copy_folder_contents(with_fixed_marker_names_folder,str(tmp_path))
-    if Path.cwd() != tmp_path:
-        os.chdir(str(tmp_path))
-    try:
-        process_and_return_model(model_type)
-    except Exception as e:
-        assert 0, "Unexpected error: {}".format(e)
 
 new_paf_fields_folder = str(
     Path("TestFiles", "WithNewPafFields").absolute())
