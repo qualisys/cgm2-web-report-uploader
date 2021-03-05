2ab6406fc3eb98298d038ad6e04ec800be865465
Sten Remmelg <sten.remmelg@gmail.com>
2021-03-05 09:05:07 +0100

    update test data since ordering changed with python version change

--- Tests/test_convert_json_to_xml.py
+++ Tests/test_convert_json_to_xml.py
@@ -1,19 +1,20 @@
-from qtmWebGaitReport.convert_report_json_to_regression_test_xml import get_xml_string_from
-from qtmWebGaitReport.convert_report_json_to_regression_test_xml import dict_to_xml
-import os
 import json
-import xmltodict
-import pytest
+import os
+from collections import OrderedDict
 
-report_json_path = os.path.join(
-    "TestFiles", "xml_test", "session_data.json")
+import pytest
+import xmltodict
+from qtmWebGaitReport.convert_report_json_to_regression_test_xml import dict_to_xml, get_xml_string_from
+
+report_json_path = os.path.join("TestFiles", "xml_test", "session_data.json")
 with open(report_json_path, "r") as f:
     report_json_data = json.load(f)
 
+resave_test_data = False
+
 
 def load_previous_session_data_xml():
-    path_to_previous_session_data = os.path.join(
-        "TestFiles", "xml_test", "session_data.xml")
+    path_to_previous_session_data = os.path.join("TestFiles", "xml_test", "session_data.xml")
     with open(path_to_previous_session_data, "r") as f:
         previous_report_xml_string = f.read()
     return previous_report_xml_string
@@ -26,4 +27,7 @@ def previous_report_xml_string():
 
 def test_check_that_xml_string_has_not_changed(previous_report_xml_string):
     report_xml_string = get_xml_string_from(report_json_data)
+    if resave_test_data:
+        with open(os.path.join("TestFiles", "xml_test", "session_data.xml"), "w") as f:
+            f.write(report_xml_string)
     assert report_xml_string == previous_report_xml_string, "xml string has changed"
