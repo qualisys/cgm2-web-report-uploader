b4e834dabdcee735a3ad01d7de20d6c1316bea92
Sten Remmelg <sten.remmelg@gmail.com>
2021-03-16 09:49:05 +0100

    add argument to change cwd in main

--- main.py
+++ main.py
@@ -1,13 +1,17 @@
-from qtmWebGaitReport.CGM_workflow_main import main as CGM_workflow_main
 import argparse
+import os
+
+from qtmWebGaitReport.CGM_workflow_main import main as CGM_workflow_main
 
 parser = argparse.ArgumentParser()
-parser.add_argument("--templates-path",
-                    default="",
-                    help="Give path to Templates directory")
-parser.add_argument('--web-report', action='store_true')
-parser.add_argument('--pdf-report', action='store_true')
+parser.add_argument("--templates-path", default="", help="Give path to Templates directory")
+parser.add_argument("--web-report", action="store_true")
+parser.add_argument("--pdf-report", action="store_true")
+parser.add_argument("--change-cwd", default="")
 args = parser.parse_args()
 
 if __name__ == "__main__":
+    if args.change_cwd != "":
+
+        os.chdir(args.change_cwd)
     CGM_workflow_main(args)
