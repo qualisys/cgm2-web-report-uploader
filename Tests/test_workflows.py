342b6b893a3896e3bbf371c6ccbeec7f3b0b91b9
Sten Remmelg <sten.remmelg@gmail.com>
2021-05-11 14:53:07 +0200

    add test data for new cgm2 versions

--- Tests/test_workflows.py
+++ Tests/test_workflows.py
@@ -27,11 +27,29 @@ def copy_folder_contents(src_dir, dst_dir):
 
 
 new_paf_fields_folder = str(Path("TestFiles", "WithNewPafFields").absolute())
-
+with_functional_knee_folder = str(Path("TestFiles", "GaitWithFunctionalKnee").absolute())
 
 @pytest.mark.slow
-def test_new_paf_fields(model_type, tmp_path):
+@pytest.mark.parametrize("model_type",["CGM1.0",
+        "CGM1.1",
+        "CGM2.1-HJC",
+        "CGM2.2-IK",
+        "CGM2.3-skinClusters",
+        "CGM2.4-ForeFoot",])
+def test_up_to_24(model_type, tmp_path):
     copy_folder_contents(new_paf_fields_folder, str(tmp_path))
+    if Path.cwd() != tmp_path:
+        os.chdir(str(tmp_path))
+    try:
+        process_and_return_model(model_type, check_events_in_mokka=False)
+    except Exception as e:
+        assert 0, "Unexpected error: {}".format(e)
+
+@pytest.mark.parametrize("model_type",[
+        "CGM2.5-UpperLimb",
+        "CGM2.6-FunctionalKnee",])
+def test_from_25(model_type, tmp_path):
+    copy_folder_contents(with_functional_knee_folder, str(tmp_path))
     if Path.cwd() != tmp_path:
         os.chdir(str(tmp_path))
     try:
