42b68071ef72c96233390800fe535f27a053b41c
Sten Remmelg <sten.remmelg@gmail.com>
2021-05-12 11:52:23 +0200

    create test for subject metadata loading

--- Tests/unit/test_session_xml.py
+++ Tests/unit/test_session_xml.py
@@ -1,9 +1,12 @@
 from pathlib import Path
 
+from qtmWebGaitReport.session_xml import create_subject_metadata, load_session_xml_soup
+
 session_xml_file = Path("TestFiles", "GaitWithFunctionalKnee", "session.xml")
 
 
 class TestParsing:
-    def test_parse_session_metadata(self):
-
-        assert 0
+    def test_parse_subject_metadata(self):
+        session_xml = load_session_xml_soup(session_xml_file)
+        metadata = create_subject_metadata(session_xml)
+        assert len(metadata) >= 13, "Some metadata seems to be missing, length is too short"
