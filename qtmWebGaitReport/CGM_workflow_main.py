dfe21e1fb168a3bdccec9a9aff24776845167949
Sten Remmelg <sten.remmelg@qualisys.se>
2024-03-21 15:16:00 +0200

    changes to main workflow

--- qtmWebGaitReport/CGM_workflow_main.py
+++ qtmWebGaitReport/CGM_workflow_main.py
@@ -11,22 +11,14 @@ from pyCGM2.Apps.QtmApps.CGMi.QPYCGM2_events import main as CGM2_events_main
 from pyCGM2.Apps.QtmApps.CGMi.QPYCGM2_processing import main as CGM2_processing_main
 from pyCGM2.Apps.QtmApps.CGMi.QPYCGM2_modelling import main as CGM2_modelling_main
 
-# from pyCGM2.Apps.QtmApps.CGMi import (
-#     CGM1_workflow,
-#     CGM11_workflow,
-#     CGM21_workflow,
-#     CGM22_workflow,
-#     CGM23_workflow,
-#     CGM24_workflow,
-#     CGM25_workflow,
-#     CGM26_workflow,
-# )
 from pyCGM2.QTM import qtmTools
 from pyCGM2.Utils import files
 
 from qtmWebGaitReport import qtmFilters
 from qtmWebGaitReport.convert_report_json_to_regression_test_xml import save_session_data_xml_from
 from qtmWebGaitReport.session_xml import SESSION_XML_FILENAME, create_subject_metadata, load_session_xml_soup
+
+import argparse
 
 
 # ---- settings-----
@@ -79,31 +71,12 @@ def delete_c3d_files_in(folder_path):
         c3d_file_path.unlink()
 
 
-# _cgm2_processing_functions = {
-#     "CGM1.0": CGM1_workflow.main,
-#     "CGM1.1": CGM11_workflow.main,
-#     "CGM2.1-HJC": CGM21_workflow.main,
-#     "CGM2.2-IK": CGM22_workflow.main,
-#     "CGM2.3-skinClusters": CGM23_workflow.main,
-#     "CGM2.4-ForeFoot": CGM24_workflow.main,
-#     "CGM2.5-UpperLimb": CGM25_workflow.main,
-#     "CGM2.6-FunctionalKnee": CGM26_workflow.main,
-# }
-
-
 # ---- process with pyCGM2-----
-def process_with_cgm2(args, model_type, generate_pdf_report=False, check_events_in_mokka=True):
+def process_with_cgm2(args):
     # model, events, processing
-    CGM2_modelling_main(args=args)  # TODO add session xml path to args
+    CGM2_modelling_main(args=args)
     CGM2_events_main(args=args)
     CGM2_processing_main(args=args)
-
-
-def process_with_pycgm(work_folder, generate_pdf_report, check_events_in_mokka=True):
-    session_xml = files.readXml(work_folder + "\\", SESSION_XML_FILENAME)
-    CGM2_Model = session_xml.Subsession.CGM2_Model.text
-    logging.info("PROCESSING TYPE " + CGM2_Model)
-    process_with_cgm2(CGM2_Model, generate_pdf_report, check_events_in_mokka=check_events_in_mokka)
 
 
 # ----web report---------
@@ -131,16 +104,15 @@ def main(args):
     processed_folder = os.path.join(work_folder, "processed")
     delete_c3d_files_in(processed_folder)
 
-    # run and process pyCGM2
-    check_events_in_mokka = True if args.skip_mokka == False else False
-    # TODO see what has happened to pdf argument
-    process_with_pycgm(
-        work_folder, args.pdf_report, check_events_in_mokka=check_events_in_mokka
-    )  # i keep  the model as output , just in case of futher use
+    # NOTE the pdf argument is fetched from session.xml in pyCGM2 code, Sten could change that
+    # NOTE the chec events in mokka feature does not seem to be in pyCGM2 anymore, Sten found some code that
+    # seems to be related to checking events in qtm, but it is not clear if it works and how it works
+    # NOTE there is now a new argument called anomalyException, that is fetched from session.xml
+    # need to decide if we want to be able to change it from qtm or just keep it on or off
+    cgm2_args = argparse.Namespace(debug=False, sessionFile=SESSION_XML_FILENAME)
+    process_with_cgm2(cgm2_args, work_folder)
 
-    webReportFlag = args.web_report
-    if webReportFlag:
-
+    if args.web_report:
         settings = load_extra_settings(args.templates_path)
         session_xml = load_session_xml_soup(Path(work_folder, SESSION_XML_FILENAME))
         create_web_report(session_xml, processed_folder, settings)
